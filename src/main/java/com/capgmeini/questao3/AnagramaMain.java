404035c90e55dd7249ed5323a226a2cabdbce7ca
Orlando Ferreira <orlandofnfilho@gmail.com>
2022-02-18 17:11:44 -0300

    update

--- src/main/java/com/capgmeini/questao3/AnagramaMain.java
+++ src/main/java/com/capgmeini/questao3/AnagramaMain.java
@@ -2,6 +2,15 @@ package com.capgmeini.questao3;
 
 import java.util.Scanner;
 
+/**
+ * Duas palavras podem ser consideradas anagramas de si mesmas se as letras de
+ * uma palavra podem ser realocadas para formar a outra palavra. Dada uma string
+ * qualquer, desenvolva um algoritmo que encontre o número de pares de
+ * substrings que são anagramas.
+ * 
+ * @author José Orlando Ferreira do N. Filho
+ */
+
 public class AnagramaMain {
 
 	public static void main(String[] args) {
